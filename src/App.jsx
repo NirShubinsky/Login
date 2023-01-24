87d6df370d77fe7924d19491721168b9bc8637d1
Nir Shubinsky <nirik11@gmail.com>
2023-01-24 02:35:04 +0200

    changed browserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  HashRouter,
 } from "react-router-dom";
 import ResetPassword from './pages/ResetPassword';
 import LoginPage from './pages/Login';
@@ -12,13 +13,13 @@ function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 -mt-16">
     <div className="max-w-md w-full space-y-8">
-     <BrowserRouter>
+     <HashRouter hashType="hashbang">
         <Routes>
             <Route path="/" element={<LoginPage/>} />
             <Route path="/reset" element={<ResetPassword/>} />
             <Route path="/newpassword" element={<NewPassword/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   </div>
   );
