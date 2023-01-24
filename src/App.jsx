af8aa9252efdb8a3c6bb75a521feb0d287208a3c
Nir Shubinsky <nirik11@gmail.com>
2023-01-24 16:00:04 +0200

    Revert "changed browserRouter to HashRouter"
    This reverts commit 87d6df370d77fe7924d19491721168b9bc8637d1.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  HashRouter,
 } from "react-router-dom";
 import ResetPassword from './pages/ResetPassword';
 import LoginPage from './pages/Login';
@@ -13,13 +12,13 @@ function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 -mt-16">
     <div className="max-w-md w-full space-y-8">
-     <HashRouter hashType="hashbang">
+     <BrowserRouter>
         <Routes>
             <Route path="/" element={<LoginPage/>} />
             <Route path="/reset" element={<ResetPassword/>} />
             <Route path="/newpassword" element={<NewPassword/>} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   </div>
   );
